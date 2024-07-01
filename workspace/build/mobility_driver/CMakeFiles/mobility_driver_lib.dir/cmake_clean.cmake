80d4f03758ccfa01d00da5c84f59b2e8cf7f5a73
Wes Fletcher <wesfletch@gmail.com>
2024-07-01 16:35:01 -0400

    initial commit

---
+++ workspace/build/mobility_driver/CMakeFiles/mobility_driver_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mobility_driver_lib.dir/src/MobilityDriver.cpp.o"
+  "CMakeFiles/mobility_driver_lib.dir/src/MobilityDriver.cpp.o.d"
+  "libmobility_driver_lib.a"
+  "libmobility_driver_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mobility_driver_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
