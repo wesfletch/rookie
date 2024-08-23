7304370c2ef78ed42ee583ab9d8854ce769c3198
Wes Fletcher <wesfletch@gmail.com>
2024-08-23 15:55:15 -0400

    heartbeat message, more dockerfile stuff, gross ROS2 shenanigans

--- workspace/src/mobility_driver/src/MobilityDriver.cpp
+++ workspace/src/mobility_driver/src/MobilityDriver.cpp
@@ -1,13 +1,21 @@
 
-// CPP headers
+// C headers
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <termios.h>
 
+// CPP headers
+#include <chrono>
+// I HATE this
+using namespace std::chrono_literals;
+#include <optional>
+
 // ROS headers
 #include <rclcpp/rclcpp.hpp>
+
 #include <std_msgs/msg/string.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 
 // Rookie-specific
 #include <mobility_driver/MobilityDriver.hpp>
@@ -19,20 +27,16 @@ namespace mobility_driver
 {
 
 
-MobilityDriver::MobilityDriver()
-    : Node("mobility_driver")
+SerialPort::~SerialPort()
 {
-    this->publisher = this->create_publisher<std_msgs::msg::String>(
-        "test_topic", 10
-    );
+    close(this->serial_port);
 }
-
 
 
 bool
 SerialPort::configure()
 {
-    int serial_port = open("/dev/ttyACM0", O_RDWR);
+    this->serial_port = ::open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);
     if (serial_port < 0) 
     {
         RCLCPP_ERROR_STREAM(rclcpp::get_logger("serial_port"),
@@ -41,7 +45,7 @@ SerialPort::configure()
     }
 
     struct termios tty;
-    if (!tcgetattr(serial_port, &tty)) 
+    if (tcgetattr(serial_port, &tty) != 0) 
     {
         RCLCPP_ERROR_STREAM(rclcpp::get_logger("serial_port"),
             "Error getting terminal settings: <" << errno << "> == " << strerror(errno));
@@ -53,14 +57,13 @@ SerialPort::configure()
     tty.c_cflag &= ~PARENB; // no parity bit
     // STOP BIT
     tty.c_cflag &= ~CSTOPB; // use ONE stop bit
-    // BITS PER BYTE
+    // (DATA) BITS PER BYTE
     tty.c_cflag &= ~CSIZE;  // first clear...
     tty.c_cflag |= CS8;     // ... then set; 8 bits per byte
     // HW FLOW CONTROL
     tty.c_cflag &= ~CRTSCTS; // disabled
     // CREAD and CLOCAL
-    tty.c_cflag |= CREAD;   // allow reading
-    tty.c_cflag |= CLOCAL;  // ignore control lines
+    tty.c_cflag |= CREAD | CLOCAL;   // allow reading, ignore control lines
 
     // Configure local modes.
     // CANONICAL-MODE: receive only on '\n' (no) or constantly (yes)
@@ -80,7 +83,7 @@ SerialPort::configure()
 
     // Configure output modes.
     tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feedtty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
 
     // VMIN/VTIME
     // We want non-blocking (polling) reads
@@ -91,7 +94,7 @@ SerialPort::configure()
     cfsetispeed(&tty, B115200);
     cfsetospeed(&tty, B115200);
 
-    if (!tcsetattr(serial_port, TCSANOW, &tty))
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0)
     {
         RCLCPP_ERROR_STREAM(rclcpp::get_logger("serial_port"),
             "Failed to set serial settings: <" << errno << "> == " << strerror(errno));
@@ -102,72 +105,193 @@ SerialPort::configure()
     return true;
 }
 
-// void
-// SerialPort::spin()
-// {
-    
-//     std::array<char> buffer[1024];
-
-//     rclcpp::Rate loop_rate(50);
-//     while (rclcpp::ok())
-//     {
-
-//         int num_bytes = read(this->serial_port, buffer.size());
-//         if (num_bytes > 0) 
-//         {
-
-            
-
-
-//         }
-        
-//         // Attempt to read input...
-//         while (ch != ENDSTDIN)
-//         {
-//             int num_bytes = read(this->serial_port, buffer.size());
-//         }
-
-//     }
-
-
-// }
-
-void
-MobilityDriver::run()
+std::optional<std::string>
+SerialPort::spinOnce()
 {
-    // Just making sure that pico_interface actually works...
-    pico_interface::Msg_MotorsCommand mtrs;
-    mtrs.motor_1_direction = pico_interface::Msg_MotorsCommand::DIRECTION::FORWARD;
-    mtrs.motor_1_pwm = 0;
-    mtrs.motor_2_direction = pico_interface::Msg_MotorsCommand::DIRECTION::FORWARD;
-    mtrs.motor_2_pwm = 0;
+    if (!this->isConfigured()) { return std::nullopt; }
 
-    std::string mtrs_out; 
-    pico_interface::message_error_t status = pico_interface::pack_MotorsCommand(
-        mtrs, mtrs_out);
-    if (status != pico_interface::message_error_t::E_MSG_SUCCESS)
+    char buffer[256];
+    memset(&buffer, '\0', sizeof(buffer));
+
+    int num_bytes = ::read(this->serial_port, &buffer, 256);
+    if (num_bytes == 0) 
     {
-        RCLCPP_ERROR_STREAM(get_logger(), "NO!");
-        return;
+        return std::nullopt; 
+    }
+    else if (num_bytes < 0) 
+    { 
+        RCLCPP_ERROR_STREAM(rclcpp::get_logger("serial_port"), 
+            "ERR " << errno << ", " << strerror(errno)); 
+        return std::nullopt; 
     }
 
-    SerialPort ser;
-    if (!ser.configure()) { return; }
+    return std::optional<std::string>{std::string(buffer)};
+}
+
+void
+SerialPort::spin()
+{
+    if (!this->isConfigured()) { return; }
+
+    char buffer[256];
+    memset(&buffer, '\0', sizeof(buffer));
+
+    std::string buffer_out;
 
     rclcpp::Rate loop_rate(50);
-    while (rclcpp::ok()) 
-    {   
-        std_msgs::msg::String msg;
-        msg.data = mtrs_out;
-        this->publisher->publish(msg);
+    while (rclcpp::ok())
+    {
+        int num_bytes = ::read(this->serial_port, &buffer, 256);
+        if (num_bytes == 0) 
+        {
+            continue; 
+        }
+        else if (num_bytes < 0) 
+        { 
+            RCLCPP_ERROR_STREAM(rclcpp::get_logger("serial_port"), 
+                "ERR " << errno << ", " << strerror(errno)); 
+            continue; 
+        }
 
-        std::cout << "LOOP" << std::endl;
+        // TODO: trying to do string accumulation here, but it's not going very well for me.
+        buffer_out += std::string(buffer);
+        if (buffer_out.find(std::string("\n")) == std::string::npos) {
+            continue;
+        }
+        buffer[num_bytes] = '\0';
+        RCLCPP_ERROR_STREAM(rclcpp::get_logger("serial_port"), 
+            "read: <" << num_bytes << ">, \"" << std::string(buffer) << "\"");
+
+        // if (!this->write(std::string("$MTR.C 1 0 1 0\n"))) { RCLCPP_ERROR_STREAM(rclcpp::get_logger("serial_port"), "FUCK"); }
         loop_rate.sleep();
     }
 }
 
 
+bool
+SerialPort::write(
+    std::string out)
+{
+    if (!this->isConfigured()) { return false; }
+
+    uint num_bytes = ::write(this->serial_port, out.c_str(), out.size());
+    if (num_bytes != out.size())
+    {
+        RCLCPP_ERROR_STREAM(rclcpp::get_logger("serial_port"), "FAILED TO WRITE");
+        return false;
+    }
+
+    return true;
+}
 
 
+
+
+
+MobilityDriver::MobilityDriver()
+{
+    this->_node = rclcpp::Node::make_shared("mobility_driver");
+
+    if (!this->serial_port.configure()) 
+    { 
+        throw std::runtime_error("Failed to configure serial port.");  
+    }
+
+    this->pico_out_pub = this->_node->create_publisher<std_msgs::msg::String>(
+        "pico_out", 10
+    );
+
+    auto vel_sub_callback = std::bind(
+        &MobilityDriver::cmdVelCallback, this, 
+        std::placeholders::_1);
+
+    this->vel_sub = this->_node->create_subscription<geometry_msgs::msg::Twist>(
+        "cmd_vel", 10, vel_sub_callback);
+
+    // Configure a timer to send heartbeat messages
+    this->heartbeat_timer = this->_node->create_wall_timer(
+        1s, 
+        std::bind(&MobilityDriver::pushHeartbeat, this));
+    
+}
+
+void
+MobilityDriver::run()
+{
+    rclcpp::Rate loop_rate(50);
+    while (rclcpp::ok()) 
+    {   
+        std::optional<std::string> out = this->serial_port.spinOnce();
+        if (out) {
+            RCLCPP_INFO_STREAM(this->_node->get_logger(), *out);
+        }
+
+        loop_rate.sleep();
+
+        rclcpp::spin_some(this->_node);
+    }
+}
+
+void
+MobilityDriver::pushHeartbeat()
+{
+    pico_interface::Msg_Heartbeat heartbeat;
+    heartbeat.seq = this->heartbeat_seq++;
+
+    std::string msg;
+    pico_interface::message_error_t result = pico_interface::pack_HeartbeatMessage(
+        heartbeat, msg);
+    if (result != pico_interface::MESSAGE_ERR::E_MSG_SUCCESS) 
+    {
+        RCLCPP_ERROR_STREAM(this->_node->get_logger(), "HEARTBEAT FUCK");
+        return;
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(this->serial_port_mtx);
+        this->serial_port.write(msg);
+    }
+}
+
+void 
+MobilityDriver::cmdVelCallback(
+    [[maybe_unused]] geometry_msgs::msg::Twist::UniquePtr msg)
+{
+    // Do the whole unicycle-model -> diff. drive song and dance number
+    // In the future, I can break this out into its own library
+    float linear_vel = msg->linear.x;
+    float angular_vel = msg->angular.z;
+
+    // TODO [WF]: I don't know these values yet, but I probably should.
+    // Go with best guess for right now...
+    float wheelbase = 0.15; // A.K.A. radius of rotation, A.K.A. 'L' in meters
+    float wheel_radius = 98.425 / 1000.0 ; // 3-7/8" --> mm --> meters
+
+    float linear_left = linear_vel - ((angular_vel * wheelbase) / 2.0);
+    float linear_right = linear_vel + ((angular_vel * wheelbase) / 2.0);
+
+    float angular_left = linear_left / wheel_radius;
+    float angular_right = linear_right / wheel_radius;
+
+    // Generate a pico_interface message from the received message
+    pico_interface::Msg_Velocity cmd;
+    cmd.motor_1_velocity = angular_left;
+    cmd.motor_2_velocity = angular_right;
+
+    std::string output;
+    pico_interface::message_error_t result = pico_interface::pack_Velocity(
+        cmd, pico_interface::MSG_ID_VELOCITY_CMD, output);
+    if (result != pico_interface::MESSAGE_ERR::E_MSG_SUCCESS) 
+    {
+        RCLCPP_ERROR_STREAM(this->_node->get_logger(), "FUCK");
+        return;
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(this->serial_port_mtx);
+        this->serial_port.write(output);
+    }
+    
+}
 
 } // namespace mobility_driver
--- workspace/src/mobility_driver/src/mobility_driver_node.cpp
+++ workspace/src/mobility_driver/src/mobility_driver_node.cpp
@@ -8,9 +8,8 @@ main(int argc, char* argv[])
     rclcpp::init(argc, argv);
     
     auto mobility_driver = std::make_shared<mobility_driver::MobilityDriver>();
-    
-    // rclcpp::spin();
     mobility_driver->run();
+    
     rclcpp::shutdown();
     
     return 0;
