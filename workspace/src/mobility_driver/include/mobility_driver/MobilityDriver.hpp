a5387de379dcd0a4797a91c95c67c05983991bf4
Wes Fletcher <wesfletch@gmail.com>
2025-03-28 16:07:54 -0400

    refactoring mobility_driver to actually work, plus or minus some weird device name issues

--- workspace/src/mobility_driver/include/mobility_driver/MobilityDriver.hpp
+++ workspace/src/mobility_driver/include/mobility_driver/MobilityDriver.hpp
@@ -14,11 +14,24 @@
 namespace mobility_driver
 {
 
+static const std::string DEFAULT_NODE_NAME = "mobility_driver";
+static const std::string DEFAULT_TOPIC_PICO_OUT = "pico_out";
+static const std::string DEFAULT_SERIAL_DEVICE_NAME = "/dev/ttyACM0";
+
+// TODO: Spin rate seems really high just for polling an MCU (which publishes at 50Hz max),
+// but if the spin rate is too low we can hang...
+static const int DEFAULT_SPIN_RATE = 200;
+
+
 class MobilityDriver
 {
 public:
-    MobilityDriver();
-    
+    MobilityDriver(
+        const std::string& node_name = DEFAULT_NODE_NAME,
+        const std::string& output_topic = DEFAULT_TOPIC_PICO_OUT,
+        const int& spin_rate = DEFAULT_SPIN_RATE,
+        const std::string& serial_device_name = DEFAULT_SERIAL_DEVICE_NAME);
+
     void run();
 
     void pushHeartbeat();
@@ -33,10 +46,11 @@ private:
 
     rclcpp::Node::SharedPtr _node;
 
+    const int spin_rate;
+
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pico_out_pub;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr vel_sub;
-
-
+        
     SerialPort serial_port;
     std::mutex serial_port_mtx;
 
