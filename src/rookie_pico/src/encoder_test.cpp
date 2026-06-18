b88270b648e699de773e330799cae0da204a03a9
Wes Fletcher <wesfletch@gmail.com>
2026-06-18 12:07:46 -0400

    move the `encoder_test` executable to using the central Pins.hpp file

--- src/rookie_pico/src/encoder_test.cpp
+++ src/rookie_pico/src/encoder_test.cpp
@@ -6,6 +6,7 @@
 #include <pico/multicore.h>
 
 #include <rookie_pico/Encoders.hpp>
+#include <rookie_pico/Pins.hpp>
 
 int
 main()
@@ -13,10 +14,14 @@ main()
     stdio_init_all();
 
     // Configure our encoders...
-    std::shared_ptr<Encoder> leftEncoder = std::make_shared<Encoder>(
-        Encoder(LEFT_CHANNEL_A_GPIO, LEFT_CHANNEL_B_GPIO, false /* invert*/));
-    std::shared_ptr<Encoder> rightEncoder = std::make_shared<Encoder>(
-        Encoder(RIGHT_CHANNEL_A_GPIO, RIGHT_CHANNEL_B_GPIO, false /* invert*/));
+    std::shared_ptr<Encoder> leftEncoder = std::make_shared<Encoder>(Encoder(
+        pins::encoders::LEFT_CHANNEL_A_GPIO,
+        pins::encoders::LEFT_CHANNEL_B_GPIO,
+        false /* invert*/));
+    std::shared_ptr<Encoder> rightEncoder = std::make_shared<Encoder>(Encoder(
+        pins::encoders::RIGHT_CHANNEL_A_GPIO,
+        pins::encoders::RIGHT_CHANNEL_B_GPIO,
+        false /* invert*/));
 
     EncoderList encoders = { leftEncoder, rightEncoder };
     init_encoders(&encoders);
